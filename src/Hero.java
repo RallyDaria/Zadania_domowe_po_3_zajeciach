1fb7abed38f4f53bf68ebd1299bf52a5e2a2844d
RallyDaria <dariazblewska@gmail.com>
2022-10-27 17:49:32 +0200

    hero

---
+++ src/Hero.java
@@ -0,0 +1,16 @@
+public class Hero {
+        String imie;
+        String klasa;
+        int punktySily;
+        int punktyMany;
+        boolean nowaPostac;
+
+    public Hero(String imie, String klasa, int punktySily, int punktyMany,boolean nowaPostac){
+        this.imie=imie;
+        this.klasa=klasa;
+        this.punktySily=punktySily;
+        this.punktyMany=punktyMany;
+        this.nowaPostac=nowaPostac;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Hero postac1 = new Hero("RallyMAN", "super", 50, 500, true);
+    }
+    System.out.print(postac1());
+}
+

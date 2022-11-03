1eec29c440e18137a3a52234225209c3ae743506
RallyDaria <dariazblewska@gmail.com>
2022-11-03 14:47:57 +0100

    aaaaaa

---
+++ src/Hero1.java
@@ -0,0 +1,66 @@
+public class Hero1 {
+        private String name;
+        private String role;
+        private int strength;
+        private int manaPoints;
+        private boolean isNew;
+
+        Hero1(String name, String role, int strength, int manaPoints, boolean isNew) {
+            this.name = name;
+            this.role = role;
+            this.strength = strength;
+            this.manaPoints = manaPoints;
+            this.isNew = isNew;
+        }
+
+        Hero1(String name, String role, boolean isNew) {
+            this.name = name;
+            this.role = role;
+            this.isNew = isNew;
+        }
+
+        public String showName() {
+            return name;
+        }
+
+        public void changeName(String name) {
+            this.name = name;
+        }
+
+        public String showRole() {
+            return role;
+        }
+
+        public void changeRole(String role) {
+            this.role = role;
+        }
+
+        public int showStrength() {
+            return strength;
+        }
+
+        public void changeStrength(int strength) {
+            this.strength = strength;
+        }
+
+        public int showManaPoints() {
+            return manaPoints;
+        }
+
+        public void changeManaPoints(int manaPoints) {
+            this.manaPoints = manaPoints;
+        }
+
+        public boolean showIsNew() {
+            return isNew;
+        }
+
+        public void changeIsNew(boolean isNew) {
+            this.isNew = isNew;
+        }
+
+        public double power(int strength, int manaPoints) {
+            return (strength * manaPoints) / 2;
+        }
+
+}

52ddacf8286b5e5406aa445c9e7977a60a67a5be
RallyDaria <dariazblewska@gmail.com>
2022-11-07 15:48:30 +0100

    Nowy Hero

--- src/Hero.java
+++ src/Hero.java
@@ -1,16 +1,52 @@
 public class Hero {
-        String imie;
-        String klasa;
-        int punktySily;
-        int punktyMany;
-        boolean nowaPostac;
+       private String name;
+       private  String klass;
+       private int punktySily;
+       private int punktyMana;
+       private boolean nowaPostac;
 
-    public Hero(String imie, String klasa, int punktySily, int punktyMany,boolean nowaPostac){
-        this.imie=imie;
-        this.klasa=klasa;
+    public Hero(String name,String klass, int punktySily, int punktyMana,boolean nowaPostac  ){
+        this.name=name;
+        this.klass=klass;
         this.punktySily=punktySily;
-        this.punktyMany=punktyMany;
+        this.punktyMana=punktyMana;
         this.nowaPostac=nowaPostac;
     }
+    Hero(String imie,String klass,boolean nowaPostac){
+        this.name=name;
+        this.klass=klass;
+        this.nowaPostac=nowaPostac;
 
+    }
+    public String showName() {
+        return name;
+    }
+    public void changeName() {
+        this.name=name;
+    }
+    public String showKlass(){
+        return klass;
+    }
+    public void changeKlass(){
+        this.klass=klass;
+    }
+    public int showPunktySily(){
+        return punktySily;
+    }
+    public void  changePunktySily(){
+        this.punktySily=punktySily;
+    }
+    public int showPunktyMana(){
+        return punktyMana;
+    }
+    public void changePunktyMana(){
+        this.punktyMana=punktyMana;
+    }
+    public boolean showNowaPostac(){
+        return nowaPostac;
+    }
+    public void changeNowaPostac(){
+        this.nowaPostac=nowaPostac;
+    }
 }
+
